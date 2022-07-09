d574b782a17da3cb0ed7ef69b1faf25143103db5
serhiilukianenkodev <serhii.lukianenko.dev@gmail.com>
2022-07-09 23:21:35 +0300

    Update task-09.js

--- js/task-09.js
+++ js/task-09.js
@@ -1,3 +1,15 @@
+const changeColorBtnEl = document.querySelector('.change-color');
+const textFieldEl = document.querySelector('.color');
+const body = document.querySelector('body');
+
+changeColorBtnEl.addEventListener('click', onChangeColorBtnClickHandler)
+
+function onChangeColorBtnClickHandler() {
+  const newColor = getRandomHexColor();
+  textFieldEl.textContent = newColor;
+  body.style.backgroundColor = newColor;
+}
+
 function getRandomHexColor() {
   return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
 }
