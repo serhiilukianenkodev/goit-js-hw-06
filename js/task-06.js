34661b068b0b908b617b3d9d63471a5bd4627bac
serhiilukianenkodev <serhii.lukianenko.dev@gmail.com>
2022-07-09 21:29:44 +0300

    Update task-06.js

---
+++ js/task-06.js
@@ -0,0 +1,16 @@
+const inputEl = document.querySelector('#validation-input');
+
+inputEl.addEventListener('blur', onInputBlurHandler);
+
+function onInputBlurHandler(event) {
+    const valeuLength = event.currentTarget.value.length;
+    const valueData = event.currentTarget.dataset.length;
+
+    if ((valeuLength >= valueData)) {
+            inputEl.classList.add('valid');
+            inputEl.classList.remove('invalid');
+        } else {
+            inputEl.classList.add('invalid');
+            inputEl.classList.remove('valid');
+        }
+}
